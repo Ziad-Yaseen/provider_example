c4fe3b5800bfbac1afb57f93c0f696caee9cad26
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-10 18:49:43 +0300

    multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_example_project/app.dart';
+import 'package:provider_example_project/state_management/cart_provider.dart';
 import 'package:provider_example_project/state_management/counter_provider_class.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => Counter(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+        ChangeNotifierProvider(create: (context) => Counter()),
+      ],
       child: const MyApp(),
     ),
   );
