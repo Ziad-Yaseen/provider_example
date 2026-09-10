fa01e41bdcfec1d04237d1ea9b9178cc585bdc58
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-10 18:37:17 +0300

    count and total where count refers to number of items and total refers to total price where single item price equals 100

--- lib/state_management/cart_provider.dart
+++ lib/state_management/cart_provider.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CartProvider with ChangeNotifier {
-  
+  int _count = 0;
+  int get count => _count;
+
+  int _total = 0;
+  int get total => _total;
+
+  void increment() {
+    _count++;
+    _total = _count * 100;
+    notifyListeners();
+  }
+
+  void decrement() {
+    if (_count <= 0) return;
+    _count--;
+    _total = count * 100;
+    notifyListeners();
+  }
+
+  void reset() {
+    if (_count <= 0) return;
+    _count = _total = 0;
+    notifyListeners();
+  }
 }
