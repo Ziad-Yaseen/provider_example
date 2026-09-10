76ed0ca146530441301bfaa513a1102c1729cb3d
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-10 18:40:10 +0300

    make home page stateless

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -4,23 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_example_project/state_management/counter_provider_class.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     log('Screen rebuild');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
